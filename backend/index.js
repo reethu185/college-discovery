e6cc8f69e32989e63d5ade8ec08236d8c7f313a8
reethu185 <reethu132006@gmail.com>
2026-04-29 22:13:52 +0530

    fix port for render

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,8 @@ app.get("/colleges/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
